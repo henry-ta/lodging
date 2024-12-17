64afc09256da57b2d527b575e99782fe4f45d68c
Henry Ta <henry.ta@tuta.io>
2024-12-16 23:16:20 -0800

    build: create Spring Boot Project
    Create Spring Boot Project with dependencies (no changes yet):
    - Spring Web
    - Spring Security
    - Spring Data JPA
    - MySQL Driver
    - Lombok
    - Validation
    
    Reference:
    - [Spring Initializr](start.spring.io)

---
+++ src/main/java/com/lodging/Lodging/LodgingApplication.java
@@ -0,0 +1,13 @@
+package com.lodging.Lodging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodgingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lodging/Lodging/LodgingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lodging.Lodging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LodgingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
